c0fdef6d9eb9ccc0245ef6bdcb04aba43071180e
Amitp0070 <amit7081483564@gmail.com>
2023-11-01 18:20:29 +0530

    data_structure and loops

---
+++ common/data_structure.py
@@ -0,0 +1,20 @@
+# name = 'Amit pathak'
+# print(name[3])
+# print(name[0:11])
+
+# x = [1, 5, 5, 8, 6, 7, 10,]
+# a =x[:7:2]
+# print("ans:", a)
+
+# c = x[::2]
+# print("ans:",c)
+
+# b = x[1::2]
+# print("ans:", b)
+
+name = "amit pathak"
+rev_name= name[::-1]
+print(rev_name)
+print(name[3::-1])
+print(name[:4:-1])
+
---
+++ common/loops.py
@@ -0,0 +1,8 @@
+movies = ["Avebgers", "Batman", "Captain america", "Joker","Black panther", "Raid", "Final destination", "Logan", "X-man," "Bahubali", "Bahubali-2", "Extraction", "Num","Fast and furious", "Uncharted"," Deadpool2", "Deadpool", "Mission-imposible", "John-Wick"]
+print(f'total movies = {len(movies)}')
+
+for name in movies:
+    print(name)
+
+    size = len(name)
+    print(f" {name} has {size} chars")

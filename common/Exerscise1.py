34f320e1dca84d9c4cccdf9c77a900ad65b01bf4
Amitp0070 <amit7081483564@gmail.com>
2023-11-01 14:47:21 +0530

    Exercise1

---
+++ common/Exerscise1.py
@@ -0,0 +1,31 @@
+'''apple = 20 
+banana= 10 
+mango = 10
+fruits = apple + banana + mango
+print("Number of the fruits:", fruits)'''
+
+
+'''Apple = 20 
+Apple = 10'''
+print("Enter the number")
+Apple = int(input())
+print("Double of apples:", Apple * 2)
+
+
+'''x= 5 
+x= x * 10
+x= x + 7
+print(x)'''
+
+'''total = 0
+total = total +1
+total = total +1
+total = total +1
+print(total )'''
+
+
+x =5 
+y =6 
+x =y*10
+y =x+7
+print(x+y)
--- common/Function2.py
+++ common/Function2.py
@@ -1,2 +1,8 @@
+def area(h, w, r):
+    cicle = pi * r ** 2
+    rectangle = h * w
+    area = circle + rectangle
+    return area
 
-
+ans =area(10, 5,5)
+print("ans:", ans)
